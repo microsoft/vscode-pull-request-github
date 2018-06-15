3a0bcd15f3d9e924449d1bc1896f85f4c313ec71
Rachel Macfarlane <ramacfar@microsoft.com>
2018-06-14 22:02:13 -0700

    Set up tests

---
+++ src/test/index.ts
@@ -0,0 +1,11 @@
+// This file is providing the test runner to use when running extension tests.
+import * as testRunner from 'vscode/lib/testrunner';
+
+// You can directly control Mocha options by uncommenting the following lines
+// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+testRunner.configure({
+    ui: 'bdd', 		// the BDD UI is being used in extension.test.ts (describe, it, etc.)
+    useColors: true // colored output from test results
+});
+
+module.exports = testRunner;
