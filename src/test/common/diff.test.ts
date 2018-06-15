e3ee681853035727b3c3901929e88c659169d8ec
Peng Lyu <penn.lv@gmail.com>
2018-06-14 21:28:51 -0700

    test for getDiffLineByPosition, mapHeadLineToDiffHunkPosition

--- src/test/common/diff.test.ts
+++ src/test/common/diff.test.ts
@@ -1,25 +1,28 @@
 import { expect } from 'chai';
 import { parseDiffHunk } from '../../common/diff';
 import { DiffLine, DiffChangeType } from '../../models/diffHunk';
+import { getDiffLineByPosition, mapHeadLineToDiffHunkPosition } from '../../common/diffPositionMapping';
 
-describe('parseDiffHunk', () => {
+const diff_hunk_0 = [
+	`@@ -1,5 +1,6 @@`,
+	` {`,
+	`     "appService.zipIgnorePattern": [`,
+	`         "node_modules{,/**}"`,
+	`-    ]`,
+	`-}`,
+	`\\ No newline at end of file`,
+	`+    ],`,
+	`+    "editor.insertSpaces": false`,
+	`+}`
+].join('\n');
+
+describe('diff hunk parsing', () => {
 	it('diffhunk iterator', () => {
-		let diffHunkReader = parseDiffHunk([
-			`@@ -1,5 +1,6 @@`,
-			` {`,
-			`     "appService.zipIgnorePattern": [`,
-			`         "node_modules{,/**}"`,
-			`-    ]`,
-			`-}`,
-			`\\ No newline at end of file`,
-			`+    ],`,
-			`+    "editor.insertSpaces": false`,
-			`+}`
-		].join('\n'));
-		let diffHunkIter = diffHunkReader.next();		
+		let diffHunkReader = parseDiffHunk(diff_hunk_0);
+		let diffHunkIter = diffHunkReader.next();
 		let diffHunk = diffHunkIter.value;
 		expect(diffHunk.diffLines.length).to.equal(9);
-		
+
 		expect(diffHunk.diffLines[0]).to.deep.equal(new DiffLine(DiffChangeType.Control, -1, -1, 0, `@@ -1,5 +1,6 @@`));
 		expect(diffHunk.diffLines[1]).to.deep.equal(new DiffLine(DiffChangeType.Context, 1, 1, 1, ` {`));
 		expect(diffHunk.diffLines[2]).to.deep.equal(new DiffLine(DiffChangeType.Context, 2, 2, 2, `     "appService.zipIgnorePattern": [`));
@@ -29,5 +32,41 @@ describe('parseDiffHunk', () => {
 		expect(diffHunk.diffLines[6]).to.deep.equal(new DiffLine(DiffChangeType.Add, -1, 4, 7, `+    ],`));
 		expect(diffHunk.diffLines[7]).to.deep.equal(new DiffLine(DiffChangeType.Add, -1, 5, 8, `+    "editor.insertSpaces": false`));
 		expect(diffHunk.diffLines[8]).to.deep.equal(new DiffLine(DiffChangeType.Add, -1, 6, 9, `+}`));
-	});	
+	});
+
+	it('getDiffLineByPosition', () => {
+		let diffHunkReader = parseDiffHunk(diff_hunk_0);
+		let diffHunkIter = diffHunkReader.next();
+		let diffHunk = diffHunkIter.value;
+
+		for (let i = 0; i < diffHunk.diffLines.length; i++) {
+			let diffLine = diffHunk.diffLines[i];
+			expect(getDiffLineByPosition([diffHunk], diffLine.positionInHunk)).to.deep.equal(diffLine, `diff line ${i}`);
+		}
+	});
+
+	it('mapHeadLineToDiffHunkPosition', () => {
+		let diffHunkReader = parseDiffHunk(diff_hunk_0);
+		let diffHunkIter = diffHunkReader.next();
+		let diffHunk = diffHunkIter.value;
+
+		for (let i = 0; i < diffHunk.diffLines.length; i++) {
+			let diffLine = diffHunk.diffLines[i];
+			switch (diffLine.type) {
+				case DiffChangeType.Delete:
+					expect(mapHeadLineToDiffHunkPosition([diffHunk], '', diffLine.oldLineNumber, true)).to.be.equal(diffLine.positionInHunk);
+					break;
+				case DiffChangeType.Add:
+					expect(mapHeadLineToDiffHunkPosition([diffHunk], '', diffLine.newLineNumber, false)).to.be.equal(diffLine.positionInHunk);
+					break;
+				case DiffChangeType.Context:
+					expect(mapHeadLineToDiffHunkPosition([diffHunk], '', diffLine.oldLineNumber, true)).to.be.equal(diffLine.positionInHunk);
+					expect(mapHeadLineToDiffHunkPosition([diffHunk], '', diffLine.newLineNumber, false)).to.be.equal(diffLine.positionInHunk);
+					break;
+
+				default:
+					break;
+			}
+		}
+	});
 });
