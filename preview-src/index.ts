61b67663331ce50b96402bfbf3a0ce2ec1418272
Peng Lyu <penn.lv@gmail.com>
2018-08-16 11:39:12 -0700

    Fix #177. Use Check out as verb.

--- preview-src/index.ts
+++ preview-src/index.ts
@@ -141,7 +141,7 @@ function updateCheckoutButton(isCheckedOut: boolean) {
 	checkoutButton.disabled = isCheckedOut;
 	checkoutMasterButton.disabled = false;
 	const activeIcon = '<svg class="octicon octicon-check" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path></svg>';
-	checkoutButton.innerHTML = isCheckedOut ? `${activeIcon} Checked Out` : `Checkout`;
+	checkoutButton.innerHTML = isCheckedOut ? `${activeIcon} Checked Out` : `Check out`;
 
 	const backButton = (<HTMLButtonElement>document.getElementById(ElementIds.CheckoutDefaultBranch));
 	if (isCheckedOut) {
