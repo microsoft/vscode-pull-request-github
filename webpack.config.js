7bbae73661793a90beea1f93dcd2c2d29bfcd79a
rebornix <penn.lv@gmail.com>
2018-08-23 15:12:21 -0700

    exclude sourcemap and ignore images in production build.

--- webpack.config.js
+++ webpack.config.js
@@ -8,70 +8,89 @@
 const path = require('path');
 const webpack = require('webpack');
 
-/** @type webpack.Configuration */
-const webview = {
-	name: 'webiew',
-	entry: {
-		index: './preview-src/index.ts'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css/,
-				use: ['style-loader', 'css-loader']
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	devtool: 'inline-source-map',
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'media')
-	}
-};
-
-/** @type webpack.Configuration */
-const extension = {
-	name: 'extension',
-	target: 'node',
-	entry: {
-		extension: './src/extension.ts'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-		alias: {
-			"node-fetch": path.resolve(__dirname, 'node_modules/node-fetch/lib/index.js'),
+function getWebviewConfig(env) {
+	/** @type webpack.Configuration */
+	let webview = {
+		name: 'webiew',
+		mode: env.production ? 'production' : 'development',
+		entry: {
+			index: './preview-src/index.ts'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				},
+				{
+					test: /\.css/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js']
+		},
+		devtool: !env.production ? 'inline-source-map' : undefined,
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'media')
 		}
-	},
-	devtool: 'source-map',
-	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'media'),
-		libraryTarget: "commonjs",
-		devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
-	},
-	externals: {
-		'vscode': 'commonjs vscode',
-		'utf-8-validate': 'utf-8-validate',
-		'bufferutil': 'bufferutil',
-		'encoding': 'encoding'
-	},
-};
+	};
 
-module.exports = [webview, extension];
+	return webview;
+}
+
+/**
+ *
+ * @param {*} env
+ * @returns webpack.Configuration
+ */
+function getExtensionConfig(env) {
+	/** @type webpack.Configuration */
+	let config = {
+		name: 'extension',
+		mode: env.production ? 'production' : 'development',
+		target: 'node',
+		entry: {
+			extension: './src/extension.ts'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+			alias: {
+				"node-fetch": path.resolve(__dirname, 'node_modules/node-fetch/lib/index.js'),
+			}
+		},
+		devtool: !env.production ? 'source-map' : undefined,
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, 'media'),
+			libraryTarget: "commonjs",
+			devtoolModuleFilenameTemplate: 'file:///[absolute-resource-path]'
+		},
+		externals: {
+			'vscode': 'commonjs vscode',
+			'utf-8-validate': 'utf-8-validate',
+			'bufferutil': 'bufferutil',
+			'encoding': 'encoding'
+		},
+	};
+
+	return config;
+}
+
+module.exports =  function(env) {
+	env = env || {};
+	env.production = !!env.production;
+	return [getExtensionConfig(env), getWebviewConfig(env)];
+};
