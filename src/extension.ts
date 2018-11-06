d8fb1c5d2467fde43716cc7d24457c2357e9f528
Peng Lyu <penn.lv@gmail.com>
2018-11-06 11:43:37 -0800

    add debug log level to help troubleshoot performance issue.

--- src/extension.ts
+++ src/extension.ts
@@ -25,6 +25,7 @@ fetch.Promise = PolyfillPromise;
 let telemetry: ITelemetry;
 
 async function init(context: vscode.ExtensionContext, git: GitAPI, repository: Repository): Promise<void> {
+	context.subscriptions.push(Logger);
 	Logger.appendLine('Git repository found, initializing review manager and pr tree view.');
 
 	const configuration = new VSCodeConfiguration();
