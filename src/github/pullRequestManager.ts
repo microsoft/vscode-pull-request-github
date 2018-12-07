2f935345db751a1f69f673430d7c70efa29163d5
Rachel Macfarlane <ramacfar@microsoft.com>
2018-12-07 15:30:44 -0800

    Ensure correct head parameter when creating pull requests

--- src/github/pullRequestManager.ts
+++ src/github/pullRequestManager.ts
@@ -571,35 +571,31 @@ export class PullRequestManager implements IPullRequestManager {
 	}
 
 	async createPullRequest(params: Github.PullRequestsCreateParams): Promise<IPullRequestModel> {
-		const repo = this.findRepo(fromHead(params));
-		await repo.ensure();
-
-		const {title, body} = titleAndBodyFrom(await this.getHeadCommitMessage());
-		if (!params.title) {
-			params.title = title;
-		}
-
-		if (!params.body) {
-			params.body = body;
-		}
-
-		// Create PR
-		let pullRequestModel;
 		try {
+			const repo = this._githubRepositories.find(r => r.remote.owner === params.owner && r.remote.repositoryName === params.repo);
+			if (!repo) {
+				throw new Error(`No matching repository ${params.repo} found for ${params.owner}`);
+			}
+
+			await repo.ensure();
+
+			const { title, body } = titleAndBodyFrom(await this.getHeadCommitMessage());
+			if (!params.title) {
+				params.title = title;
+			}
+
+			if (!params.body) {
+				params.body = body;
+			}
+
+			// Create PR
 			let { data } = await repo.octokit.pullRequests.create(params);
-			pullRequestModel = await repo.getPullRequest(data.number);
+			return await repo.getPullRequest(data.number);
 		} catch (e) {
 			Logger.appendLine(`GitHubRepository> Creating pull requests failed: ${e}`);
 			vscode.window.showWarningMessage(`Creating pull requests for '${params.head}' failed: ${formatError(e)}`);
 			return null;
 		}
-
-		if (pullRequestModel) {
-			await PullRequestGitHelper.fetchAndCheckout(this._repository, pullRequestModel.remote, params.head, pullRequestModel);
-			return pullRequestModel;
-		}
-
-		return null;
 	}
 
 	async editIssueComment(pullRequest: IPullRequestModel, commentId: string, text: string): Promise<Comment> {
@@ -981,18 +977,6 @@ const ownedByMe: Predicate<GitHubRepository> = repo => {
 
 const byRemoteName = (name: string): Predicate<GitHubRepository> =>
 	({remote: {remoteName}}) => remoteName === name;
-
-const fromHead = (params: Github.PullRequestsCreateParams): Predicate<GitHubRepository> => {
-	const {head, repo} = params;
-	const idxSep = head.indexOf(':');
-	const owner = idxSep !== -1
-		? head.substr(0, idxSep)
-		: params.owner;
-	return byOwnerAndName(owner, repo);
-};
-
-const byOwnerAndName = (byOwner: string, repo: string): Predicate<GitHubRepository> =>
-	({remote: {owner, repositoryName}}) => byOwner === owner && repo === repositoryName;
 
 const titleAndBodyFrom = (message: string): {title: string, body: string} => {
 	const idxLineBreak = message.indexOf('\n');
