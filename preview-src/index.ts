8400ea3b0a6a8f2d61b027276059d81fe8e40270
Miguel Solorio <miguel.solorio@microsoft.com>
2019-01-25 15:49:59 -0800

    Remove extra word

--- preview-src/index.ts
+++ preview-src/index.ts
@@ -168,7 +168,7 @@ function renderDescription(pr: PullRequest): HTMLElement {
 
 	const commentHeader = document.createElement('div');
 	commentHeader.classList.add('comment-header');
-	commentHeader.innerHTML = `<div class="comment-info"><img class="avatar" src="${pr.author.avatarUrl}" alt=""> <span><a href="${pr.author.url}">${pr.author.login}</a> commented on <a href=${pr.url} class="timestamp">${dateFromNow(pr.createdAt)}</a></span></div>`;
+	commentHeader.innerHTML = `<div class="comment-info"><img class="avatar" src="${pr.author.avatarUrl}" alt=""> <span><a href="${pr.author.url}">${pr.author.login}</a> commented <a href=${pr.url} class="timestamp">${dateFromNow(pr.createdAt)}</a></span></div>`;
 
 	const commentBody = document.createElement('div');
 	commentBody.classList.add('description-content');
