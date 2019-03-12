f4252ff717a689bb674275a924686d8ca6cfea96
Peng Lyu <penn.lv@gmail.com>
2019-03-11 17:20:50 -0700

    Pending but not draft.

--- src/github/utils.ts
+++ src/github/utils.ts
@@ -25,7 +25,7 @@ export function convertToVSCodeComment(comment: Comment, command: vscode.Command
 		command: command,
 		userName: comment.user!.login,
 		userIconPath: vscode.Uri.parse(comment.user!.avatarUrl),
-		label: !!comment.isDraft ? 'Draft' : undefined,
+		label: !!comment.isDraft ? 'Pending' : undefined,
 		commentReactions: comment.reactions ? comment.reactions.map(reaction => {
 			return { label: reaction.label, hasReacted: reaction.viewerHasReacted, count: reaction.count, iconPath: reaction.icon };
 		}) : []
