5b3ef00a2486cf63b0bdec71ab779519c2aa1f4e
RMacfarlane <ramacfar@microsoft.com>
2019-03-28 15:01:55 -0700

    Add more logging around isGithub

--- src/authentication/githubServer.ts
+++ src/authentication/githubServer.ts
@@ -61,9 +61,11 @@ export class GitHubManager {
 
 			get.end();
 			get.on('error', err => {
+				Logger.appendLine(`No response from host ${host}: ${err.message}`, 'GitHubServer');
 				resolve(false);
 			});
 		}).then(isGitHub => {
+			Logger.debug(`Host ${host} is associated with GitHub: ${isGitHub}`, 'GitHubServer');
 			this._servers.set(host.authority, isGitHub);
 			return isGitHub;
 		});
