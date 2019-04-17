6ba55ba187125a90cbba6da6d2f8c41d7fb01cce
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-04-17 13:58:06 -0400

    Attempting a fix

--- preview-src/user.tsx
+++ preview-src/user.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { PullRequest } from './cache';
+import { Resource } from '../src/common/resources';
 
 export const Avatar = ({ for: author }: { for: Partial<PullRequest['author']> }) =>
 	<a className='avatar-link' href={author.url}>
-		<img className='avatar' src={author.avatarUrl} alt='' />
+		<img className='avatar' src={author.avatarUrl ? author.avatarUrl : Resource.icons.light.Avatar} alt='' />
 	</a>;
 
 export const AuthorLink = ({ for: author, text=author.login }: { for: PullRequest['author'], text?: string }) =>
