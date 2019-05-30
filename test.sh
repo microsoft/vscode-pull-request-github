18ea0b45560009f6dd012bb2f8fbfa6a42773262
Ash Wilson <smashwilson@github.com>
2019-05-30 16:17:51 -0400

    Transpile preview-src/ and resources/

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+printf "Transpiling src/\n"
 tsc -p .
 scripts/preprocess-gql --in src/github/queries.gql --out out/github/queries.gql
+
+printf "\nTranspiling preview-src/\n"
+tsc -p preview-src/
+scripts/preprocess-svg --in resources/ --out preview-src/dist/resources/
+
+printf "\nLaunching test runner\n"
 node ./node_modules/vscode/bin/test
