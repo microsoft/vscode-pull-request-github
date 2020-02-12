9f4a2f8ddd2ca6efe279e79590cb6e4d28da20d2
Alex Ross <alros@microsoft.com>
2020-02-12 11:39:22 +0100

    Add max line length and line count for issue links
    Part of #1489

--- src/issues/issueLinkProvider.ts
+++ src/issues/issueLinkProvider.ts
@@ -8,23 +8,33 @@ import * as LRUCache from 'lru-cache';
 import * as vscode from 'vscode';
 import { getIssue, ISSUE_EXPRESSION, ParsedIssue } from './util';
 
-export class IssueLinkProvider implements vscode.DocumentLinkProvider {
-	private linkMap: Map<vscode.DocumentLink, { value: string, parsed: ParsedIssue }> = new Map();
+const MAX_LINE_COUNT = 2000;
+const MAX_LINE_LENGTH = 150;
 
+class IssueDocumentLink extends vscode.DocumentLink {
+	constructor(range: vscode.Range, public readonly mappedLink: { readonly value: string, readonly parsed: ParsedIssue }) {
+		super(range);
+	}
+}
+
+export class IssueLinkProvider implements vscode.DocumentLinkProvider {
 	constructor(private manager: PullRequestManager, private resolvedIssues: LRUCache<string, PullRequestModel>) { }
 
 	provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.DocumentLink[]> {
 		const links: vscode.DocumentLink[] = [];
-		for (let i = 0; i < document.lineCount; i++) {
+		const wraps: boolean = vscode.workspace.getConfiguration('editor', document).get('wordWrap', 'off') !== 'off';
+		for (let i = 0; i < Math.min(document.lineCount, MAX_LINE_COUNT); i++) {
 			let searchResult = -1;
 			let lineOffset = 0;
-			let lineSubstring = document.lineAt(i).text;
+			const line = document.lineAt(i).text;
+			const lineLength = wraps ? line.length : Math.min(line.length, MAX_LINE_LENGTH);
+			let lineSubstring = line.substring(0, lineLength);
 			while ((searchResult = lineSubstring.search(ISSUE_EXPRESSION)) >= 0) {
 				const match = lineSubstring.match(ISSUE_EXPRESSION);
 				if (match && (match.length > 1)) {
-					const link = new vscode.DocumentLink(new vscode.Range(new vscode.Position(i, searchResult + lineOffset), new vscode.Position(i, searchResult + lineOffset + match[0].length)));
+					const link = new IssueDocumentLink(new vscode.Range(new vscode.Position(i, searchResult + lineOffset), new vscode.Position(i, searchResult + lineOffset + match[0].length)),
+						{ value: match[0], parsed: { owner: match[2], name: match[3], issueNumber: parseInt(match[4]) } });
 					links.push(link);
-					this.linkMap.set(link, { value: match[0], parsed: { owner: match[2], name: match[3], issueNumber: parseInt(match[4]) } });
 				}
 				lineOffset += searchResult + (match ? match[0].length : 0) + 1;
 				lineSubstring = lineSubstring.substring(lineOffset, lineSubstring.length);
@@ -33,13 +43,10 @@ export class IssueLinkProvider implements vscode.DocumentLinkProvider {
 		return links;
 	}
 
-	async resolveDocumentLink(link: vscode.DocumentLink, _token: vscode.CancellationToken): Promise<vscode.DocumentLink | undefined> {
-		const mappedLink = this.linkMap.get(link);
-		if (mappedLink) {
-			const issue = await getIssue(this.resolvedIssues, this.manager, mappedLink.value, mappedLink.parsed);
-			if (issue) {
-				link.target = await vscode.env.asExternalUri(vscode.Uri.parse(issue.html_url));
-			}
+	async resolveDocumentLink(link: IssueDocumentLink, _token: vscode.CancellationToken): Promise<vscode.DocumentLink | undefined> {
+		const issue = await getIssue(this.resolvedIssues, this.manager, link.mappedLink.value, link.mappedLink.parsed);
+		if (issue) {
+			link.target = await vscode.env.asExternalUri(vscode.Uri.parse(issue.html_url));
 		}
 		return link;
 	}
