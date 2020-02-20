3fb3a2e3c0218aba8045b30ed2c7a044973959e6
Alex Ross <alros@microsoft.com>
2020-02-20 15:03:41 +0100

    Only show issues in suggest when previous character is '#' and suggest is manually triggered

--- src/issues/issueCompletionProvider.ts
+++ src/issues/issueCompletionProvider.ts
@@ -77,6 +77,11 @@ export class IssueCompletionProvider implements vscode.CompletionItemProvider {
 	}
 
 	async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext): Promise<vscode.CompletionItem[]> {
+		// If the suggest was not triggered by the trigger character, require that the previous character be the trigger character
+		if ((position.character > 0) && (context.triggerKind === vscode.CompletionTriggerKind.Invoke) && (document.getText(new vscode.Range(position.with(undefined, position.character - 1), position)) !== '#')) {
+			return [];
+		}
+
 		const milestones = await this._items;
 		let range: vscode.Range = new vscode.Range(position, position);
 		if (position.character - 1 >= 0) {
