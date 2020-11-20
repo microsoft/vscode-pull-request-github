fff4e80d522131fd0bf018cec70426505c6eafde
Alex Ross <alros@microsoft.com>
2020-11-20 10:07:53 +0100

    Fix build in response to API proposal changes

--- src/view/prsTreeDataProvider.ts
+++ src/view/prsTreeDataProvider.ts
@@ -33,7 +33,7 @@ export class PullRequestsTreeDataProvider implements vscode.TreeDataProvider<Tre
 	) {
 		this._disposables = [];
 		this._disposables.push(vscode.workspace.registerTextDocumentContentProvider('pr', getInMemPRContentProvider()));
-		this._disposables.push(vscode.window.registerDecorationProvider(DecorationProvider));
+		this._disposables.push(vscode.window.registerFileDecorationProvider(DecorationProvider));
 		this._disposables.push(vscode.commands.registerCommand('pr.refreshList', _ => {
 			this._onDidChangeTreeData.fire();
 		}));
