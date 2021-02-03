b2c48c5638fa32444bccdfc2aeca301d405c4567
RMacfarlane <ramacfar@microsoft.com>
2021-02-03 11:14:04 -0800

    PR: Using inputbox warnings as showing errors is not accessible

--- webviews/createPullRequestView/app.tsx
+++ webviews/createPullRequestView/app.tsx
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as React from 'react';
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { render } from 'react-dom';
 import PullRequestContext, { CreateParams } from '../common/createContext';
 import { gitCompareIcon, repoIcon } from '../components/icon';
@@ -14,6 +14,8 @@ export function main() {
 		<Root>{(params: CreateParams) => {
 			const ctx = useContext(PullRequestContext);
 			const [isBusy, setBusy] = useState(false);
+
+			const titleInput = useRef<HTMLInputElement>();
 
 			function updateBaseBranch(branch: string): void {
 				ctx.changeBaseBranch(branch);
@@ -28,7 +30,12 @@ export function main() {
 
 			async function create(): Promise<void> {
 				setBusy(true);
-				await ctx.submit();
+				const hasValidTitle = await ctx.validate();
+				if (!hasValidTitle) {
+					titleInput.current.focus();
+				} else {
+					await ctx.submit();
+				}
 				setBusy(false);
 			}
 
@@ -67,21 +74,32 @@ export function main() {
 				</div>
 
 				<div className='wrapper'>
-					<input type='text' name='title' className={params.showTitleValidationError ? 'input-error' : ''} placeholder='Pull Request Title' value={params.pendingTitle} required onChange={(e) => updateTitle(e.currentTarget.value)}></input>
-					<div className={params.showTitleValidationError ? 'validation-error below-input-error' : 'hidden'}>A title is required.</div>
+					<input
+						type='text'
+						ref={titleInput}
+						name='title'
+						className={params.showTitleValidationError ? 'input-error' : ''}
+						aria-invalid={!!params.showTitleValidationError}
+						aria-describedBy={params.showTitleValidationError ? 'title-error' : ''}
+						placeholder='Pull Request Title'
+						value={params.pendingTitle}
+						required
+						onChange={(e) => updateTitle(e.currentTarget.value)}>
+					</input>
+					<div id='title-error' className={params.showTitleValidationError ? 'validation-error below-input-error' : 'hidden'}>A title is required.</div>
 				</div>
 
 				<div className='wrapper'>
 					<textarea name='description' placeholder='Pull Request Description' value={params.pendingDescription} required onChange={(e) => ctx.updateState({ pendingDescription: e.currentTarget.value })}></textarea>
 				</div>
 
-				<div className={params.validate && !!params.createError ? 'wrapper validation-error' : 'hidden'}>
+				<div className={params.validate && !!params.createError ? 'wrapper validation-error' : 'hidden'} aria-live='assertive'>
 					{params.createError}
 				</div>
 
 				<div className='wrapper flex'>
-					<input type='checkbox' name='draft' checked={params.isDraft} onClick={() => ctx.updateState({ isDraft: !params.isDraft })}></input>
-					<label htmlFor='checkbox'>Create as draft</label>
+					<input id='draft-checkbox' type='checkbox' name='draft' checked={params.isDraft} onClick={() => ctx.updateState({ isDraft: !params.isDraft })}></input>
+					<label htmlFor='draft-checkbox'>Create as draft</label>
 				</div>
 
 				<div className='actions'>
