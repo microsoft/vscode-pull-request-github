1f39f31c03de60521c15951440be23ef89c51bd3
Ankit Sinha <ankitbko@gmail.com>
2021-06-24 19:07:02 +0530

    removed url check (#56)
    Co-authored-by: Ankit Sinha <anksinha@microsoft.com>

--- src/authentication/azdoServer.ts
+++ src/authentication/azdoServer.ts
@@ -1,23 +1,10 @@
 import * as vscode from 'vscode';
 
 export class AzdoManager {
-	// TODO WTF Does this do?
-	private _servers: Map<string, boolean> = new Map()
-		.set('dev.azure.com', true)
-		.set('ssh.dev.azure.com', true)
-		.set('vs-ssh.visualstudio.com', true);
-
 	public async isAzdo(host: vscode.Uri): Promise<boolean> {
 		if (host === null) {
 			return false;
 		}
-
-		for (const [key, value] of this._servers) {
-			if (key.includes(host.authority)) {
-				return value;
-			}
-		}
-
-		return host.authority.includes('.visualstudio.com');
+		return true;
 	}
 }
