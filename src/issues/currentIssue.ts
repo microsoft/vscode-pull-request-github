12333ec81b198afdddf55cd59f417a10600c9b68
Alex Ross <alros@microsoft.com>
2021-08-23 11:14:11 +0200

    Revert "Fixes 2259"
    This reverts commit d85622edbd57139689d0e519640c3ab150da5cb2.

--- src/issues/currentIssue.ts
+++ src/issues/currentIssue.ts
@@ -123,19 +123,6 @@ export class CurrentIssue {
 		return false;
 	}
 
-	private async renameBranch(oldBranch: string | undefined, newBranch: string): Promise<Boolean> {
-		try {
-			if (oldBranch && await this.branchExists(oldBranch)) {
-				await vscode.commands.executeCommand('git.renameBranch', [newBranch]);
-			}
-		} catch (e) {
-			vscode.window.showErrorMessage(
-				`Unable to rename branch ${oldBranch}. Git error: ${e.error}`,
-			);
-		}
-		return true;
-	}
-
 	private async createOrCheckoutBranch(branch: string): Promise<boolean> {
 		try {
 			if (await this.branchExists(branch)) {
@@ -214,19 +201,13 @@ export class CurrentIssue {
 		}
 		const state: IssueState = this.stateManager.getSavedIssueState(this.issueModel.number);
 		this._branchName = this.shouldPromptForBranch ? undefined : state.branch;
-		const branchNameConfig = await variableSubstitution(
-			await this.ensureBranchTitleConfigMigrated(),
-			this.issue,
-			undefined,
-			await this.getUser(),
-		);
-		if (this._branchName !== branchNameConfig) {
-			const branchExists = await this.branchExists(this._branchName!);
-			if (!branchExists) {
-				this._branchName = branchNameConfig;
-			}
-		}
 		if (!this._branchName) {
+			const branchNameConfig = await variableSubstitution(
+				await this.ensureBranchTitleConfigMigrated(),
+				this.issue,
+				undefined,
+				await this.getUser(),
+			);
 			if (createBranchConfig === 'on') {
 				const validateBranchName = this.validateBranchName(branchNameConfig);
 				if (validateBranchName) {
