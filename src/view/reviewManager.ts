6c2682fde0dfa6b542951602bcfff8631beac2d5
Alex Ross <alros@microsoft.com>
2021-08-23 13:35:28 +0200

    Fix extra place where silent update doesn't happen

--- src/view/reviewManager.ts
+++ src/view/reviewManager.ts
@@ -145,7 +145,7 @@ export class ReviewManager {
 					// For subsequent changes, we don't want to make visible updates.
 					// This occurs on branch changes.
 					// Note that the visible changes will occur when checking out a PR.
-					this.updateState(!!oldHead);
+					this.updateState(true);
 				}
 			}),
 		);
