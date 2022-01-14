1dce43a43b04379f45fe89a39ebb4178d0666868
Joshua O'Brien <joshua.obrien@tutanota.com>
2022-01-14 23:10:30 +1000

    Add optional opts as arg to openDiff

--- src/view/treeNodes/fileChangeNode.ts
+++ src/view/treeNodes/fileChangeNode.ts
@@ -304,12 +304,15 @@ export class FileChangeNode extends TreeNode implements vscode.TreeItem {
 		return openFileCommand(this.filePath, opts);
 	}
 
-	async openDiff(folderManager: FolderRepositoryManager): Promise<void> {
+	async openDiff(folderManager: FolderRepositoryManager, _opts?: vscode.TextDocumentShowOptions): Promise<void> {
 		const command = await openDiffCommand(
 			folderManager,
 			this.parentFilePath,
 			this.filePath,
-			this.opts,
+			{
+				...this.opts,
+				..._opts,
+			},
 			this.status,
 		);
 		vscode.commands.executeCommand(command.command, ...(command.arguments ?? []));
