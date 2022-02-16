e83eb5881993df2aa26989b60caf59779d8635fa
Alex Ross <alros@microsoft.com>
2022-02-16 10:59:50 +0100

    Don't show colors on viewed files

--- src/view/fileTypeDecorationProvider.ts
+++ src/view/fileTypeDecorationProvider.ts
@@ -4,15 +4,68 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import { ViewedState } from '../common/comment';
 import { GitChangeType } from '../common/file';
 import { fromFileChangeNodeUri, fromPRUri } from '../common/uri';
+import { FolderRepositoryManager } from '../github/folderRepositoryManager';
+import { RepositoriesManager } from '../github/repositoriesManager';
+import { ReviewManager } from './reviewManager';
 
 export class FileTypeDecorationProvider implements vscode.FileDecorationProvider {
-	private _disposables: vscode.Disposable[];
+	private _disposables: vscode.Disposable[] = [];
+	private _activePullRequestListeners: vscode.Disposable[] = [];
+	private _fileViewedListeners: Map<FolderRepositoryManager, vscode.Disposable[]> = new Map();
 
-	constructor() {
-		this._disposables = [];
+	_onDidChangeFileDecorations: vscode.EventEmitter<vscode.Uri | vscode.Uri[]> = new vscode.EventEmitter<
+		vscode.Uri | vscode.Uri[]
+	>();
+	onDidChangeFileDecorations: vscode.Event<vscode.Uri | vscode.Uri[]> = this._onDidChangeFileDecorations.event;
+
+
+	constructor(private _repositoriesManager: RepositoriesManager, private _reviewManagers: ReviewManager[]) {
 		this._disposables.push(vscode.window.registerFileDecorationProvider(this));
+		this._registerListeners();
+	}
+
+	private _registerFileViewedListeners(folderManager: FolderRepositoryManager) {
+		let viewedListeners: vscode.Disposable[] = [];
+		if (this._fileViewedListeners.has(folderManager)) {
+			this._fileViewedListeners.get(folderManager)?.forEach(disposable => disposable.dispose());
+		}
+		this._fileViewedListeners.set(folderManager, viewedListeners);
+		if (folderManager.activePullRequest) {
+			viewedListeners.push(folderManager.activePullRequest.onDidChangeFileViewedState(changed => {
+				this._onDidChangeFileDecorations.fire(changed.changed.map(change => {
+					const reviewManager = ReviewManager.getReviewManagerForFolderManager(this._reviewManagers, folderManager);
+					const fileChangeNode = reviewManager?.reviewModel.localFileChangesMap.get(change.fileName);
+					return fileChangeNode!.resourceUri;
+				}));
+			}));
+		}
+	}
+
+	private _registerActivePullRequestListeners() {
+		this._activePullRequestListeners.forEach(disposable => disposable.dispose());
+		this._fileViewedListeners.forEach(dispose => dispose.forEach(dispose => dispose.dispose()));
+		this._activePullRequestListeners.push(...this._repositoriesManager.folderManagers.map(folderManager => {
+			this._registerFileViewedListeners(folderManager);
+			return folderManager.onDidChangeActivePullRequest(() => {
+				this._registerFileViewedListeners(folderManager);
+			});
+		}));
+	}
+
+	private _registerListeners() {
+		this._registerActivePullRequestListeners();
+		this._disposables.push(this._repositoriesManager.onDidChangeFolderRepositories(() => {
+			this._registerActivePullRequestListeners();
+		}));
+
+	}
+
+	private getViewedState(fileName: string, uri: vscode.Uri) {
+		const manager = this._repositoriesManager.getManagerForFile(uri);
+		return manager?.activePullRequest?.fileChangeViewedState[fileName] ?? ViewedState.UNVIEWED;
 	}
 
 	provideFileDecoration(
@@ -28,7 +81,7 @@ export class FileTypeDecorationProvider implements vscode.FileDecorationProvider
 			return {
 				propagate: false,
 				badge: this.letter(fileChangeUriParams.status),
-				color: this.color(fileChangeUriParams.status)
+				color: this.color(fileChangeUriParams.status, this.getViewedState(fileChangeUriParams.fileName, uri))
 			};
 		}
 
@@ -45,7 +98,11 @@ export class FileTypeDecorationProvider implements vscode.FileDecorationProvider
 		return undefined;
 	}
 
-	color(status: GitChangeType): vscode.ThemeColor | undefined {
+	color(status: GitChangeType, viewedState?: ViewedState): vscode.ThemeColor | undefined {
+		if (viewedState === ViewedState.VIEWED) {
+			return undefined;
+		}
+
 		let color: string | undefined;
 		switch (status) {
 			case GitChangeType.MODIFY:
@@ -91,5 +148,7 @@ export class FileTypeDecorationProvider implements vscode.FileDecorationProvider
 
 	dispose() {
 		this._disposables.forEach(dispose => dispose.dispose());
+		this._activePullRequestListeners.forEach(dispose => dispose.dispose());
+		this._fileViewedListeners.forEach(dispose => dispose.forEach(dispose => dispose.dispose()));
 	}
 }
--- src/view/reviewModel.ts
+++ src/view/reviewModel.ts
@@ -7,6 +7,7 @@ import * as vscode from 'vscode';
 import { GitFileChangeNode, RemoteFileChangeNode } from './treeNodes/fileChangeNode';
 
 export class ReviewModel {
+	private _localFileChangesMap: Map<string, GitFileChangeNode> | undefined;
 	private _localFileChanges: GitFileChangeNode[] | undefined;
 	private _onDidChangeLocalFileChanges: vscode.EventEmitter<void> = new vscode.EventEmitter();
 	public onDidChangeLocalFileChanges: vscode.Event<void> = this._onDidChangeLocalFileChanges.event;
@@ -24,6 +25,7 @@ export class ReviewModel {
 	}
 
 	set localFileChanges(localFileChanges: GitFileChangeNode[]) {
+		this._localFileChangesMap = undefined;
 		this._localFileChanges = localFileChanges;
 		this._onDidChangeLocalFileChanges.fire();
 	}
@@ -34,6 +36,16 @@ export class ReviewModel {
 
 	set obsoleteFileChanges(obsoleteFileChanges: (GitFileChangeNode | RemoteFileChangeNode)[]) {
 		this._obsoleteFileChanges = obsoleteFileChanges;
+	}
+
+	get localFileChangesMap(): Map<string, GitFileChangeNode> {
+		if (!this._localFileChangesMap) {
+			this._localFileChangesMap = new Map();
+			this._localFileChanges?.forEach(change => {
+				this._localFileChangesMap?.set(change.fileName, change);
+			});
+		}
+		return this._localFileChangesMap;
 	}
 
 	clear() {
