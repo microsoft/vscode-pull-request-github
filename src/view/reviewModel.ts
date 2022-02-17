0a75702ea7a52a2fb746ce699c151b4a74c34949
Alex Ross <alros@microsoft.com>
2022-02-17 11:10:02 +0100

    Cleanup

--- src/view/reviewModel.ts
+++ src/view/reviewModel.ts
@@ -7,7 +7,6 @@ import * as vscode from 'vscode';
 import { GitFileChangeNode, RemoteFileChangeNode } from './treeNodes/fileChangeNode';
 
 export class ReviewModel {
-	private _localFileChangesMap: Map<string, GitFileChangeNode> | undefined;
 	private _localFileChanges: GitFileChangeNode[] | undefined;
 	private _onDidChangeLocalFileChanges: vscode.EventEmitter<void> = new vscode.EventEmitter();
 	public onDidChangeLocalFileChanges: vscode.Event<void> = this._onDidChangeLocalFileChanges.event;
@@ -25,7 +24,6 @@ export class ReviewModel {
 	}
 
 	set localFileChanges(localFileChanges: GitFileChangeNode[]) {
-		this._localFileChangesMap = undefined;
 		this._localFileChanges = localFileChanges;
 		this._onDidChangeLocalFileChanges.fire();
 	}
@@ -36,16 +34,6 @@ export class ReviewModel {
 
 	set obsoleteFileChanges(obsoleteFileChanges: (GitFileChangeNode | RemoteFileChangeNode)[]) {
 		this._obsoleteFileChanges = obsoleteFileChanges;
-	}
-
-	get localFileChangesMap(): Map<string, GitFileChangeNode> {
-		if (!this._localFileChangesMap) {
-			this._localFileChangesMap = new Map();
-			this._localFileChanges?.forEach(change => {
-				this._localFileChangesMap?.set(change.fileName, change);
-			});
-		}
-		return this._localFileChangesMap;
 	}
 
 	clear() {
