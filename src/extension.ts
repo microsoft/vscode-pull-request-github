f0fdf3802f4dc8723da333b04bb894021ed4b43b
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-08-22 14:08:56 +0200

    Update post-commit command (#3873)

--- src/extension.ts
+++ src/extension.ts
@@ -282,7 +282,9 @@ function registerPostCommitCommandsProvider(reposManager: RepositoriesManager, g
 			Logger.debug(`Found ${found ? 'a repo' : 'no repos'} when getting post commit commands.`, 'GitPostCommitCommands');
 			return found ? [{
 				command: 'pr.create',
-				title: 'Commit & Create Pull Request'
+				title: '$(git-pull-request-create) Commit',
+				description: '$(git-pull-request-create) Commit & Create Pull Request',
+				tooltip: 'Commit & Create Pull Request'
 			}] : [];
 		}
 	}
