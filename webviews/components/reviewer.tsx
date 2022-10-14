a2fa4cb759949b4fb57a4648a3ebe0e2f867f767
David Dossett <ddossett@microsoft.com>
2022-10-13 20:57:45 -0700

    Fix review state icon alignment

--- webviews/components/reviewer.tsx
+++ webviews/components/reviewer.tsx
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import React, { cloneElement, useContext, useState } from 'react';
+import React, { cloneElement, useContext } from 'react';
 import { ReviewState } from '../../src/github/interface';
 import { default as PullRequestContext } from '../common/context';
 import { checkIcon, commentIcon, deleteIcon, diffIcon, pendingIcon } from './icon';
@@ -10,31 +10,28 @@ import { AuthorLink, Avatar } from './user';
 
 export function Reviewer(reviewState: ReviewState & { canDelete: boolean }) {
 	const { reviewer, state, canDelete } = reviewState;
-	const [showDelete, setShowDelete] = useState(false);
 	const { removeReviewer } = useContext(PullRequestContext);
 	return (
-		<div
-			className="section-item reviewer"
-			onMouseEnter={state === 'REQUESTED' ? () => setShowDelete(true) : null}
-			onMouseLeave={state === 'REQUESTED' ? () => setShowDelete(false) : null}
-		>
+		<div className="section-item reviewer">
 			<div className="avatar-with-author">
 				<Avatar for={reviewer} />
 				<AuthorLink for={reviewer} />
 			</div>
-			{canDelete && showDelete ? (
-				<button className="remove-item" onClick={() => removeReviewer(reviewState.reviewer.login)}>
-					{deleteIcon}️
-				</button>
-			) : null}
-			{REVIEW_STATE[state]}
+			<div className="reviewer-icons">
+				{canDelete && (
+					<button className="icon-button" onClick={() => removeReviewer(reviewState.reviewer.login)}>
+						{deleteIcon}️
+					</button>
+				)}
+				{REVIEW_STATE[state]}
+			</div>
 		</div>
 	);
 }
 
 const REVIEW_STATE: { [state: string]: React.ReactElement } = {
-	REQUESTED: cloneElement(pendingIcon, { className: 'push-right', title: 'Awaiting requested review' }),
-	COMMENTED: cloneElement(commentIcon, { className: 'push-right', Root: 'div', title: 'Left review comments' }),
-	APPROVED: cloneElement(checkIcon, { className: 'push-right', title: 'Approved these changes' }),
-	CHANGES_REQUESTED: cloneElement(diffIcon, { className: 'push-right', title: 'Requested changes' }),
+	REQUESTED: cloneElement(pendingIcon, { className: 'section-icon', title: 'Awaiting requested review' }),
+	COMMENTED: cloneElement(commentIcon, { className: 'section-icon', Root: 'div', title: 'Left review comments' }),
+	APPROVED: cloneElement(checkIcon, { className: 'section-icon', title: 'Approved these changes' }),
+	CHANGES_REQUESTED: cloneElement(diffIcon, { className: 'section-icon', title: 'Requested changes' }),
 };
