dbaf1e09e32ca30e689f14d01d442a3f61778439
Alex Ross <alros@microsoft.com>
2022-11-14 17:12:26 +0100

    Fix tests

--- src/test/view/prsTree.test.ts
+++ src/test/view/prsTree.test.ts
@@ -202,14 +202,14 @@ describe('GitHub Pull Requests view', function () {
 			assert.strictEqual(localChildren.length, 2);
 			const [localItem0, localItem1] = localChildren.map(node => node.getTreeItem());
 
-			assert.strictEqual(localItem0.label, '#1111: zero');
+			assert.strictEqual(localItem0.label, 'zero');
 			assert.strictEqual(localItem0.tooltip, 'zero by @me');
 			assert.strictEqual(localItem0.description, 'by @me');
 			assert.strictEqual(localItem0.collapsibleState, vscode.TreeItemCollapsibleState.Collapsed);
 			assert.strictEqual(localItem0.contextValue, 'pullrequest:local:nonactive');
 			assert.deepStrictEqual(localItem0.iconPath!.toString(), 'https://avatars.com/me.jpg&s=64');
 
-			assert.strictEqual(localItem1.label, '✓ #2222: one');
+			assert.strictEqual(localItem1.label, '✓ one');
 			assert.strictEqual(localItem1.tooltip, 'Current Branch * one by @you');
 			assert.strictEqual(localItem1.description, 'by @you');
 			assert.strictEqual(localItem1.collapsibleState, vscode.TreeItemCollapsibleState.Collapsed);
