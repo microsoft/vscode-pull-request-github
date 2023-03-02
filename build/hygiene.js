58519d0b8feb6bf63dedd88c1c26e21e8cb7a586
SUKALPA108 <71346070+SUKALPA108@users.noreply.github.com>
2023-03-02 20:02:17 +0530

    Update hygiene.js

--- build/hygiene.js
+++ build/hygiene.js
@@ -15,10 +15,7 @@ const pall = require('p-all');
 const { all, tsFormattingFilter, copyrightFilter, unicodeFilter, indentationFilter } = require('./filters');
 
 const copyrightHeaderLines = [
-	'/*---------------------------------------------------------------------------------------------',
-	' *  Copyright (c) Microsoft Corporation. All rights reserved.',
-	' *  Licensed under the MIT License. See License.txt in the project root for license information.',
-	' *--------------------------------------------------------------------------------------------*/',
+	
 ];
 
 function hygiene(some) {
