b655e45bb97b13c3db91e58eb6f82efed90b1163
Alex Ross <alros@microsoft.com>
2023-03-09 15:32:02 +0100

    Add editor navigation menu entry for diff (#4602)
    * Add editor navigation menu entry for diff
    
    * Preserve visible range

--- src/commands.ts
+++ src/commands.ts
@@ -297,29 +297,48 @@ export function registerCommands(
 		}),
 	);
 
+	async function openDiffView(fileChangeNode: GitFileChangeNode | InMemFileChangeNode | vscode.Uri | undefined) {
+		if (fileChangeNode && !(fileChangeNode instanceof vscode.Uri)) {
+			const folderManager = reposManager.getManagerForIssueModel(fileChangeNode.pullRequest);
+			if (!folderManager) {
+				return;
+			}
+			return fileChangeNode.openDiff(folderManager);
+		} else if (fileChangeNode || vscode.window.activeTextEditor) {
+			const editor = fileChangeNode ? vscode.window.visibleTextEditors.find(editor => editor.document.uri.toString() === fileChangeNode.toString())! : vscode.window.activeTextEditor!;
+			const visibleRanges = editor.visibleRanges;
+			const folderManager = reposManager.getManagerForFile(editor.document.uri);
+			if (!folderManager?.activePullRequest) {
+				return;
+			}
+			const reviewManager = ReviewManager.getReviewManagerForFolderManager(reviewManagers, folderManager);
+			if (!reviewManager) {
+				return;
+			}
+			const change = reviewManager.reviewModel.localFileChanges.find(change => change.resourceUri.with({ query: '' }).toString() === editor.document.uri.toString());
+			await change?.openDiff(folderManager);
+			const tabInput = vscode.window.tabGroups.activeTabGroup.activeTab?.input;
+			const diffEditor = (tabInput instanceof vscode.TabInputTextDiff && tabInput.modified.toString() === editor.document.uri.toString()) ? vscode.window.activeTextEditor : undefined;
+			if (diffEditor) {
+				diffEditor.revealRange(visibleRanges[0]);
+			}
+		}
+	}
+
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
 			'pr.openDiffView',
 			(fileChangeNode: GitFileChangeNode | InMemFileChangeNode | undefined) => {
-				if (fileChangeNode) {
-					const folderManager = reposManager.getManagerForIssueModel(fileChangeNode.pullRequest);
-					if (!folderManager) {
-						return;
-					}
-					fileChangeNode.openDiff(folderManager);
-				} else if (vscode.window.activeTextEditor) {
-					const activeTextEditor = vscode.window.activeTextEditor;
-					const folderManager = reposManager.getManagerForFile(activeTextEditor.document.uri);
-					if (!folderManager?.activePullRequest) {
-						return;
-					}
-					const reviewManager = ReviewManager.getReviewManagerForFolderManager(reviewManagers, folderManager);
-					if (!reviewManager) {
-						return;
-					}
-					const change = reviewManager.reviewModel.localFileChanges.find(change => change.resourceUri.with({ query: '' }).toString() === activeTextEditor.document.uri.toString());
-					change?.openDiff(folderManager);
-				}
+				return openDiffView(fileChangeNode);
+			},
+		),
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			'pr.openDiffViewFromEditor',
+			(uri: vscode.Uri) => {
+				return openDiffView(uri);
 			},
 		),
 	);
