50a20de81f1190ee76d2ab865324370f9b518237
Alex Ross <alros@microsoft.com>
2023-04-28 14:59:07 +0200

    Remove path

--- src/github/repositoriesManager.ts
+++ src/github/repositoriesManager.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as path from 'path';
 import * as vscode from 'vscode';
 import { Repository } from '../api/api';
 import { AuthProvider } from '../common/authentication';
