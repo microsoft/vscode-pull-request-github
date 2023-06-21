75ab102822a4e6c44d4ba7461383ce968004f960
Hylke Bons <hi@planetpeanut.uk>
2023-06-21 10:57:00 +0200

    pr: Update icon references

--- webviews/components/icon.tsx
+++ webviews/components/icon.tsx
@@ -31,3 +31,10 @@ export const requestChanges = <Icon src={require('../../resources/icons/request_
 export const settingsIcon = <Icon src={require('../../resources/icons/settings.svg')} />;
 export const closeIcon = <Icon src={require('../../resources/icons/close.svg')} />;
 export const syncIcon = <Icon src={require('../../resources/icons/sync.svg')} />;
+export const prBaseIcon = <Icon src={require('../../resources/icons/pr_base.svg')} />;
+export const prMergeIcon = <Icon src={require('../../resources/icons/pr_merge.svg')} />;
+export const gearIcon = <Icon src={require('../../resources/icons/gear.svg')} />;
+export const assigneeIcon = <Icon src={require('../../resources/icons/assignee.svg')} />;
+export const reviewerIcon = <Icon src={require('../../resources/icons/reviewer.svg')} />;
+export const labelIcon = <Icon src={require('../../resources/icons/label.svg')} />;
+export const milestoneIcon = <Icon src={require('../../resources/icons/milestone.svg')} />;
