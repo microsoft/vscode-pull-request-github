8e4356ee5f7856b5c321141bceea60f72ed1ea2a
Alex Ross <alros@microsoft.com>
2023-07-10 12:37:16 +0200

    Buffer is not defined when adding labels (#5025)
    Part of #5009

--- src/github/issueOverview.ts
+++ src/github/issueOverview.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
+import { Buffer } from 'buffer';
 import * as vscode from 'vscode';
 import { IComment } from '../common/comment';
 import Logger from '../common/logger';
