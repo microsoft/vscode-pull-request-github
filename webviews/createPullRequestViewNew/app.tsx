0c6ff0a1e6c5f957c283abf5282e48e504d57c3b
Hylke Bons <hi@planetpeanut.uk>
2023-07-26 15:32:54 +0200

    create: Use input style for branch buttons (#5125)

--- webviews/createPullRequestViewNew/app.tsx
+++ webviews/createPullRequestViewNew/app.tsx
@@ -288,7 +288,7 @@ export function main() {
 								{createMethodLabel(ctx.createParams.isDraft, ctx.createParams.autoMerge, ctx.createParams.autoMergeMethod).label}
 							</button>
 							<div className='split'></div>
-							<button className='split-right' title='Create Actions' disabled={isBusy || !isCreateable || !ctx.initialized} onClick={(e) => {
+							<button className='split-right' title='Create with Option' disabled={isBusy || !isCreateable || !ctx.initialized} onClick={(e) => {
 								e.preventDefault();
 								const rect = (e.target as HTMLElement).getBoundingClientRect();
 								const x = rect.left;
