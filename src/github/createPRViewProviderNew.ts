e78de7c838421221cfe64265022c9c7ff3f05fff
Alex Ross <alros@microsoft.com>
2023-10-27 10:45:59 +0200

    Fix compareHasUpstream async issue Fixes #5380

--- src/github/createPRViewProviderNew.ts
+++ src/github/createPRViewProviderNew.ts
@@ -699,7 +699,7 @@ export class CreatePullRequestViewProviderNew extends WebviewViewBase implements
 			try {
 				let commits: string[];
 				let patches: string[];
-				if (this.model.compareHasUpstream) {
+				if (await this.model.getCompareHasUpstream()) {
 					commits = (await this.model.gitHubCommits()).map(commit => commit.commit.message);
 					patches = (await this.model.gitHubFiles()).map(file => file.patch ?? '');
 				} else {
