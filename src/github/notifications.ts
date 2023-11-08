7b42b1080871fa2f4a253eadb1290c84347660a2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-11-08 14:19:58 -0500

    spelling: thread
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/github/notifications.ts
+++ src/github/notifications.ts
@@ -255,13 +255,13 @@ export class NotificationProvider implements vscode.Disposable {
 		return { data: data, headers: headers };
 	}
 
-	private async markNotificationThreadAsRead(thredId) {
+	private async markNotificationThreadAsRead(threadId) {
 		const github = this.getGitHub();
 		if (!github) {
 			return;
 		}
 		await github.octokit.call(github.octokit.api.activity.markThreadAsRead, {
-			thread_id: thredId
+			thread_id: threadId
 		});
 	}
 
