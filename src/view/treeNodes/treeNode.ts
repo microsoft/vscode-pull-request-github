83bdbc24460ab146d211d560c8d07ead84ddf98a
Joshua O'Brien <joshua.obrien@tutanota.com>
2023-12-03 16:19:21 +1100

    add static processCheckboxUpdates helper to tree node

--- src/view/treeNodes/treeNode.ts
+++ src/view/treeNodes/treeNode.ts
@@ -6,6 +6,7 @@
 import * as vscode from 'vscode';
 import Logger from '../../common/logger';
 import { dispose } from '../../common/utils';
+import { FileChangeNode } from './fileChangeNode';
 
 export interface BaseTreeNode {
 	reveal(element: TreeNode, options?: { select?: boolean; focus?: boolean; expand?: boolean | number }): Thenable<void>;
@@ -65,6 +66,37 @@ export abstract class TreeNode implements vscode.Disposable {
 	}
 
 	updateFromCheckboxChanged(_newState: vscode.TreeItemCheckboxState): void { }
+
+	static processCheckboxUpdates(checkboxUpdates: vscode.TreeCheckboxChangeEvent<TreeNode>) {
+			const checkedNodes: FileChangeNode[] = [];
+			const uncheckedNodes: FileChangeNode[] = [];
+
+			checkboxUpdates.items.forEach(checkboxUpdate => {
+				const node = checkboxUpdate[0];
+				const newState = checkboxUpdate[1];
+
+				if (node instanceof FileChangeNode) {
+					if (newState == vscode.TreeItemCheckboxState.Checked) {
+						checkedNodes.push(node);
+					} else {
+						uncheckedNodes.push(node);
+					}
+				}
+
+				node.updateFromCheckboxChanged(newState);
+			});
+
+			if (checkedNodes.length > 0) {
+				const prModel = checkedNodes[0].pullRequest;
+				const filenames = checkedNodes.map(n => n.fileName);
+				prModel.markFilesAsViewed(filenames, true);
+			}
+			if (uncheckedNodes.length > 0) {
+				const prModel = checkedNodes[0].pullRequest;
+				const filenames = checkedNodes.map(n => n.fileName);
+				prModel.unmarkFilesAsViewed(filenames, true);
+			}
+	}
 
 	dispose(): void {
 		if (this.childrenDisposables) {
