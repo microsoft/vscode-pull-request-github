e3e3fe80c9a083fadf58937c65203d0df0b6cfd3
Alex Ross <alros@microsoft.com>
2024-01-11 12:57:44 +0100

    [Accessibility] Semantic heading tag is missing in issue and PR webview comments Fixes #5524

--- webviews/components/comment.tsx
+++ webviews/components/comment.tsx
@@ -148,7 +148,7 @@ function CommentBox({ for: comment, onFocus, onMouseEnter, onMouseLeave, childre
 	return (
 		<div className="comment-container comment review-comment" {...{ onFocus, onMouseEnter, onMouseLeave }}>
 			<div className="review-comment-container">
-				<div className="review-comment-header">
+				<h3 className="review-comment-header">
 					<Spaced>
 						<Avatar for={author} />
 						<AuthorLink for={author} />
@@ -170,7 +170,7 @@ function CommentBox({ for: comment, onFocus, onMouseEnter, onMouseLeave, childre
 							</>
 						) : null}
 					</Spaced>
-				</div>
+				</h3>
 				{children}
 			</div>
 		</div>
