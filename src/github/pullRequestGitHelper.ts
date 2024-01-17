95583a84ebabef3c97da912bf5f757a8a0b72560
Alex Ross <alros@microsoft.com>
2024-01-17 15:17:13 +0100

    Option to merge `main` into the checked out PR (#5628)
    Fixes #4375

--- src/github/pullRequestGitHelper.ts
+++ src/github/pullRequestGitHelper.ts
@@ -12,7 +12,7 @@ import { GitErrorCodes } from '../api/api1';
 import Logger from '../common/logger';
 import { Protocol } from '../common/protocol';
 import { parseRepositoryRemotes, Remote } from '../common/remote';
-import { PR_SETTINGS_NAMESPACE, PULL_PR_BRANCH_BEFORE_CHECKOUT } from '../common/settingKeys';
+import { PR_SETTINGS_NAMESPACE, PULL_PR_BRANCH_BEFORE_CHECKOUT, PullPRBranchVariants } from '../common/settingKeys';
 import { IResolvedPullRequestModel, PullRequestModel } from './pullRequestModel';
 
 const PullRequestRemoteMetadataKey = 'github-pr-remote';
@@ -198,7 +198,8 @@ export class PullRequestGitHelper {
 			await repository.checkout(branchName);
 
 			// respect the git setting to fetch before checkout
-			if (vscode.workspace.getConfiguration(PR_SETTINGS_NAMESPACE).get<boolean>(PULL_PR_BRANCH_BEFORE_CHECKOUT, true)) {
+			const settingValue = vscode.workspace.getConfiguration(PR_SETTINGS_NAMESPACE).get<PullPRBranchVariants>(PULL_PR_BRANCH_BEFORE_CHECKOUT, 'pull');
+			if (settingValue === 'pull' || settingValue === 'pullAndUpdateBase' || settingValue === true) {
 				const remote = readConfig(`branch.${branchName}.remote`);
 				const ref = readConfig(`branch.${branchName}.merge`);
 				progress.report({ message: vscode.l10n.t('Fetching branch {0}', branchName) });
