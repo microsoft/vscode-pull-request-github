f5c6649f7a6cd85a67140f4bc8cd2a5567162da9
Alex Ross <alros@microsoft.com>
2024-01-26 11:36:25 +0100

    A11y_Visual Studio Code Web Extensions _GitHub Pull request_ Create Pull request_ Screen Reader: Narrator is not announcing the state of Expanded/collapsed for "Create with Option" arrow button Fixes #5483

--- webviews/components/contextDropdown.tsx
+++ webviews/components/contextDropdown.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import React from 'react';
+import React, { useState } from 'react';
 import { chevronDownIcon } from './icon';
 
 interface ContextDropdownProps {
@@ -17,6 +17,20 @@ interface ContextDropdownProps {
 }
 
 export const ContextDropdown = ({ optionsContext, defaultOptionLabel, defaultOptionValue, defaultAction, optionsTitle, disabled, hasSingleAction }: ContextDropdownProps) => {
+	const [expanded, setExpanded] = useState(false);
+	const onHideAction = (e: MouseEvent | KeyboardEvent) => {
+		if (e.target instanceof HTMLElement && e.target.classList.contains('split-right')) {
+			return;
+		}
+		setExpanded(false);
+	};
+	document.onclick = (e) => {
+		onHideAction(e);
+	};
+
+	document.onkeydown = (e) => {
+		onHideAction(e);
+	};
 	return <div className='primary-split-button'>
 		<button className='split-left' disabled={disabled} onClick={defaultAction} value={defaultOptionValue()}
 			title={defaultOptionLabel()}>
@@ -24,14 +38,21 @@ export const ContextDropdown = ({ optionsContext, defaultOptionLabel, defaultOpt
 		</button>
 		<div className='split'></div>
 		{hasSingleAction ? null :
-			<button className='split-right' title={optionsTitle} disabled={disabled} onClick={(e) => {
+			<button className='split-right' title={optionsTitle} disabled={disabled} aria-expanded={expanded} onClick={(e) => {
 				e.preventDefault();
 				const rect = (e.target as HTMLElement).getBoundingClientRect();
 				const x = rect.left;
 				const y = rect.bottom;
 				e.target.dispatchEvent(new MouseEvent('contextmenu', { bubbles: true, clientX: x, clientY: y }));
 				e.stopPropagation();
-			}} data-vscode-context={optionsContext()}>
+			}}
+			onMouseDown={() => setExpanded(true)}
+			onKeyDown={(e) => {
+				if (e.key === 'Enter' || e.key === ' ') {
+					setExpanded(true);
+				}
+			}}
+			data-vscode-context={optionsContext()}>
 				{chevronDownIcon}
 			</button>
 		}
