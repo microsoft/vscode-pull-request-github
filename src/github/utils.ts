ea0930dba7ada0f67a2162e542fe13da5a9e287d
Alex Ross <alros@microsoft.com>
2024-03-08 16:21:47 +0100

    Improve outdated comments experience (#5818)
    Fixes #5760

--- src/github/utils.ts
+++ src/github/utils.ts
@@ -112,6 +112,9 @@ export function createVSCodeCommentThreadForReviewThread(
 	} else if (thread.viewerCanUnresolve && thread.isResolved) {
 		vscodeThread.contextValue = 'canUnresolve';
 	}
+	if (thread.isOutdated) {
+		vscodeThread.contextValue += 'outdated';
+	}
 
 	updateCommentThreadLabel(vscodeThread as GHPRCommentThread);
 	vscodeThread.collapsibleState = getCommentCollapsibleState(thread, undefined, currentUser);
@@ -128,7 +131,7 @@ export const COMMENT_EXPAND_STATE_COLLAPSE_VALUE = 'collapseAll';
 export const COMMENT_EXPAND_STATE_EXPAND_VALUE = 'expandUnresolved';
 export function getCommentCollapsibleState(thread: IReviewThread, expand?: boolean, currentUser?: string) {
 	if (thread.isResolved
-		|| (currentUser && (thread.comments[thread.comments.length - 1].user?.login === currentUser))) {
+		|| (!thread.isOutdated && currentUser && (thread.comments[thread.comments.length - 1].user?.login === currentUser))) {
 		return vscode.CommentThreadCollapsibleState.Collapsed;
 	}
 	if (expand === undefined) {
