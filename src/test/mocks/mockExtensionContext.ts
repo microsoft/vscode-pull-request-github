78b7ef9f8953f898962a4f8722363b774c270a77
Alex Ross <alros@microsoft.com>
2024-05-21 11:19:11 +0200

    Fix build from change in VS Code API

--- src/test/mocks/mockExtensionContext.ts
+++ src/test/mocks/mockExtensionContext.ts
@@ -1,6 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import * as path from 'path';
 import * as temp from 'temp';
-import { ExtensionContext, Uri, SecretStorage, Event, SecretStorageChangeEvent } from 'vscode';
+import { ExtensionContext, Uri, SecretStorage, Event, SecretStorageChangeEvent, EventEmitter } from 'vscode';
 
 import { InMemoryMemento } from './inMemoryMemento';
 
@@ -39,6 +44,13 @@ export class MockExtensionContext implements ExtensionContext {
 	extensionRuntime: any;
 	extension: any;
 	isNewInstall: any;
+	languageModelAccessInformation = {
+		onDidChange: new EventEmitter<void>().event,
+
+		canSendRequest: (_chat: any) => {
+			return undefined;
+		}
+	};
 
 	constructor() {
 		this.extensionPath = path.resolve(__dirname, '..');
