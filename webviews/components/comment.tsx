f6aa0bd6a4ce010c1b2eb0f2eeccc91f53199666
ixzhao <96172341+ixzhao@users.noreply.github.com>
2024-09-17 23:25:27 +0800

    fix quote reply (#6230)
    * fix quote reply
    
    * Two new lines before reply
    
    ---------
    
    Co-authored-by: Alex Ross <alros@microsoft.com>

--- webviews/components/comment.tsx
+++ webviews/components/comment.tsx
@@ -310,7 +310,7 @@ export function AddComment({
 	const textareaRef = useRef<HTMLTextAreaElement>();
 
 	emitter.addListener('quoteReply', (message: string) => {
-		const quoted = message.replace(/\n\n/g, '\n\n> ');
+		const quoted = message.replace(/\n/g, '\n> ');
 		updatePR({ pendingCommentText: `> ${quoted} \n\n` });
 		textareaRef.current?.scrollIntoView();
 		textareaRef.current?.focus();
