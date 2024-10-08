c17ca7cef6583b7c0f538ce7da54887ab0885a67
Alex Ross <alros@microsoft.com>
2024-10-08 15:37:18 +0200

    Choose first repo if none provided in issue tool

--- src/lm/issueTool.ts
+++ src/lm/issueTool.ts
@@ -5,11 +5,12 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { FolderRepositoryManager } from '../github/folderRepositoryManager';
 import { RepositoriesManager } from '../github/repositoriesManager';
 
 interface IssueToolParameters {
 	issueNumber: number;
-	repo: {
+	repo?: {
 		owner: string;
 		name: string;
 	};
@@ -27,13 +28,25 @@ export class IssueTool implements vscode.LanguageModelTool<IssueToolParameters> 
 	constructor(private readonly repositoriesManager: RepositoriesManager) { }
 
 	async invoke(options: vscode.LanguageModelToolInvocationOptions<IssueToolParameters>, _token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult | undefined> {
-		const folderManager = this.repositoriesManager.getManagerForRepository(options.parameters.repo.owner, options.parameters.repo.name);
-		if (!folderManager) {
-			throw new Error(`No folder manager found for ${options.parameters.repo.owner}/${options.parameters.repo.name}. Make sure to have the repository open.`);
+		let owner: string | undefined;
+		let name: string | undefined;
+		let folderManager: FolderRepositoryManager | undefined;
+		// The llm likes to make up an owner and name if it isn't provided one, and they tend to include 'owner' and 'name' respectively
+		if (options.parameters.repo && !options.parameters.repo.owner.includes('owner') && !options.parameters.repo.name.includes('name')) {
+			owner = options.parameters.repo.owner;
+			name = options.parameters.repo.name;
+			folderManager = this.repositoriesManager.getManagerForRepository(options.parameters.repo.owner, options.parameters.repo.name);
+		} else if (this.repositoriesManager.folderManagers.length > 0) {
+			folderManager = this.repositoriesManager.folderManagers[0];
+			owner = folderManager.gitHubRepositories[0].remote.owner;
+			name = folderManager.gitHubRepositories[0].remote.repositoryName;
 		}
-		const issue = await folderManager.resolveIssue(options.parameters.repo.owner, options.parameters.repo.name, options.parameters.issueNumber, true);
+		if (!folderManager || !owner || !name) {
+			throw new Error(`No folder manager found for ${owner}/${name}. Make sure to have the repository open.`);
+		}
+		const issue = await folderManager.resolveIssue(owner, name, options.parameters.issueNumber, true);
 		if (!issue) {
-			throw new Error(`No issue found for ${options.parameters.repo.owner}/${options.parameters.repo.name}/${options.parameters.issueNumber}. Make sure the issue exists.`);
+			throw new Error(`No issue found for ${owner}/${name}/${options.parameters.issueNumber}. Make sure the issue exists.`);
 		}
 		const result: IssueResult = {
 			title: issue.title,
