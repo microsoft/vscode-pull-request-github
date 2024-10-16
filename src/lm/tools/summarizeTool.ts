624968dafb35c0db0aa4f0763ff15983c4318153
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-10-16 16:07:11 +0200

    adding additional patches

--- src/lm/tools/summarizeTool.ts
+++ src/lm/tools/summarizeTool.ts
@@ -5,51 +5,56 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import { FetchResult } from './fetchTool';
 import { concatAsyncIterable } from './toolsUtils';
 
-interface SummarizationToolParameters {
-	title: string;
-	body: string;
-	comments: {
-		body: string;
-	}[];
-}
-
-export class SummarizationTool implements vscode.LanguageModelTool<SummarizationToolParameters> {
+export class SummarizationTool implements vscode.LanguageModelTool<FetchResult> {
 
 	constructor() { }
 
-	async invoke(options: vscode.LanguageModelToolInvocationOptions<SummarizationToolParameters>, _token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult | undefined> {
+	async invoke(options: vscode.LanguageModelToolInvocationOptions<FetchResult>, _token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult | undefined> {
 		let issueOrPullRequestInfo: string = `
 Title : ${options.parameters.title}
 Body : ${options.parameters.body}
 `;
-		const comments = options.parameters.comments;
-		for (const [index, comment] of comments.entries()) {
+		const fileChanges = options.parameters.fileChanges;
+		if (fileChanges) {
 			issueOrPullRequestInfo += `
+The following are the files changed:
+`;
+			for (const fileChange of fileChanges.values()) {
+				issueOrPullRequestInfo += `
+File : ${fileChange.fileName}
+Patch: ${fileChange.patch}
+`;
+			}
+			const comments = options.parameters.comments;
+			for (const [index, comment] of comments.entries()) {
+				issueOrPullRequestInfo += `
 Comment ${index} :
 Body: ${comment.body}
 `;
-		}
-		const models = await vscode.lm.selectChatModels({
-			vendor: 'copilot',
-			family: 'gpt-4o'
-		});
-		const model = models[0];
+			}
+			const models = await vscode.lm.selectChatModels({
+				vendor: 'copilot',
+				family: 'gpt-4o'
+			});
+			const model = models[0];
 
-		if (model) {
-			const messages = [vscode.LanguageModelChatMessage.User(summarizeInstructions())];
-			messages.push(vscode.LanguageModelChatMessage.User(`The issue or pull request information is as follows:`));
-			messages.push(vscode.LanguageModelChatMessage.User(issueOrPullRequestInfo));
-			const response = await model.sendRequest(messages, {});
-			const responseText = await concatAsyncIterable(response.text);
-			return {
-				'text/plain': responseText
-			};
-		} else {
-			return {
-				'text/plain': issueOrPullRequestInfo
-			};
+			if (model) {
+				const messages = [vscode.LanguageModelChatMessage.User(summarizeInstructions())];
+				messages.push(vscode.LanguageModelChatMessage.User(`The issue or pull request information is as follows:`));
+				messages.push(vscode.LanguageModelChatMessage.User(issueOrPullRequestInfo));
+				const response = await model.sendRequest(messages, {});
+				const responseText = await concatAsyncIterable(response.text);
+				return {
+					'text/plain': responseText
+				};
+			} else {
+				return {
+					'text/plain': issueOrPullRequestInfo
+				};
+			}
 		}
 	}
 }
@@ -57,6 +62,9 @@ Body: ${comment.body}
 function summarizeInstructions(): string {
 	return `
 You are an AI assistant who is very proficient in summarizing issues and PRs.
-You will be given information relative to an issue or PR. Your task is to output a summary of the information. Make sure the summary is at least as short or shorter than the issue or PR with the comments.
+You will be given information relative to an issue or PR : the title, the body and the comments. In the case of a PR you will also be given patches of the PR changes.
+Your task is to output a summary of all this information.
+Do not output code. When you try to summarize PR changes, write in a textual format.
+Make sure the summary is at least as short or shorter than the issue or PR with the comments and the patches if there are.
 `;
 }
