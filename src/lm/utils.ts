40fc7ee667665620f7e532ebd9529ee480b42c78
Alex Ross <alros@microsoft.com>
2024-10-24 11:07:45 +0200

    Make Copilot settings real settings (#6407)
    * Make Copilot settings real settings
    
    * PR feedback!

--- src/lm/utils.ts
+++ src/lm/utils.ts
@@ -8,7 +8,7 @@ import { commands } from '../common/executeCommands';
 import { EXPERIMENTAL_USE_QUICK_CHAT, PR_SETTINGS_NAMESPACE } from '../common/settingKeys';
 
 export function chatCommand(): typeof commands.QUICK_CHAT_OPEN | typeof commands.OPEN_CHAT {
-	const useQuickChat = vscode.workspace.getConfiguration(PR_SETTINGS_NAMESPACE).get<boolean>(EXPERIMENTAL_USE_QUICK_CHAT, true);
+	const useQuickChat = vscode.workspace.getConfiguration(PR_SETTINGS_NAMESPACE).get<boolean>(EXPERIMENTAL_USE_QUICK_CHAT, false);
 	if (useQuickChat) {
 		return commands.QUICK_CHAT_OPEN;
 	}
