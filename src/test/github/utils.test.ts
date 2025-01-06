1635031bd4f233bfbdfdc71b5bc024d98e51ef88
Alex Ross <alros@microsoft.com>
2025-01-06 15:13:45 +0100

    Fix test

--- src/test/github/utils.test.ts
+++ src/test/github/utils.test.ts
@@ -10,11 +10,10 @@ describe('utils', () => {
 
 	describe('getPRFetchQuery', () => {
 		it('replaces all instances of ${user}', () => {
-			const repo = 'microsoft/vscode-pull-request-github';
 			const user = 'rmacfarlane';
 			const query = 'reviewed-by:${user} -author:${user}';
-			const result = getPRFetchQuery(repo, user, query)
-			assert.strictEqual(result, 'is:pull-request reviewed-by:rmacfarlane -author:rmacfarlane type:pr repo:microsoft/vscode-pull-request-github');
+			const result = getPRFetchQuery(user, query)
+			assert.strictEqual(result, 'is:pull-request reviewed-by:rmacfarlane -author:rmacfarlane type:pr');
 		});
 	});
 
