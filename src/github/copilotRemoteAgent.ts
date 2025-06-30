2e0ccce2dbe0150475663bc4c06e9c0db14305cc
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-06-30 11:17:38 -0700

    update llmstring (#7145)

--- src/github/copilotRemoteAgent.ts
+++ src/github/copilotRemoteAgent.ts
@@ -386,7 +386,7 @@ export class CopilotRemoteAgentManager extends Disposable {
 			const { pull_request } = await capiClient.postRemoteAgentJob(owner, repo, payload);
 			this._onDidCreatePullRequest.fire(pull_request.number);
 			const webviewUri = await toOpenPullRequestWebviewUri({ owner, repo, pullRequestNumber: pull_request.number });
-			const prLlmString = `The remote agent has begun work. The user can track progress by visiting ${pull_request.html_url} or from the PR extension. Format this VS Code webview link so the user can click it to also track progress: ${webviewUri.toString()}`;
+			const prLlmString = `The remote agent has begun work. The user can track progress on GitHub.com by visiting ${pull_request.html_url} and within VS Code by visiting ${webviewUri.toString()}. Format all links as markdown (eg: [link text](url)).`;
 			return {
 				state: 'success',
 				number: pull_request.number,
