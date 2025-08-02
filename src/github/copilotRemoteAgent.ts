64d2abd4bc7b619ef281442cd746b36d9907874a
Peng Lyu <penn.lv@gmail.com>
2025-08-01 19:04:05 -0700

    update file changes cache after new session completes (#7486)

--- src/github/copilotRemoteAgent.ts
+++ src/github/copilotRemoteAgent.ts
@@ -1198,6 +1198,7 @@ export class CopilotRemoteAgentManager extends Disposable {
 				isCompleted = true;
 				cancellationListener?.dispose();
 
+				await pullRequest.getFileChangesInfo();
 				const multiDiffPart = await this.getFileChangesMultiDiffPart(pullRequest);
 				if (multiDiffPart) {
 					stream.push(multiDiffPart);
