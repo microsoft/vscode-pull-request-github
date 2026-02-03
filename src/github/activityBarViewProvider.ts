5677ffc177aee4d70fed659a99dbd3472025c412
Alex Ross <38270282+alexr00@users.noreply.github.com>
2026-02-03 12:50:42 +0100

    Copilot PR feedback

--- src/github/activityBarViewProvider.ts
+++ src/github/activityBarViewProvider.ts
@@ -203,9 +203,13 @@ export class PullRequestViewProvider extends WebviewViewBase implements vscode.W
 				this._folderRepositoryManager.mergeQueueMethodForBranch(pullRequestModel.base.ref, pullRequestModel.remote.owner, pullRequestModel.remote.repositoryName),
 				ensureEmojis(this._folderRepositoryManager.context),
 			]);
-			this._updatingPromise = updatingPromise;
+			const clearingPromise = updatingPromise.finally(() => {
+				if (this._updatingPromise === clearingPromise) {
+					this._updatingPromise = undefined;
+				}
+			});
+			this._updatingPromise = clearingPromise;
 			const [pullRequest, repositoryAccess, timelineEvents, requestedReviewers, branchInfo, defaultBranch, currentUser, viewerCanEdit, hasReviewDraft, coAuthors, mergeQueueMethod] = await updatingPromise;
-			this._updatingPromise = undefined;
 
 			if (!pullRequest) {
 				throw new Error(
@@ -308,8 +312,6 @@ export class PullRequestViewProvider extends WebviewViewBase implements vscode.W
 
 		} catch (e) {
 			vscode.window.showErrorMessage(`Error updating active pull request view: ${formatError(e)}`);
-		} finally {
-			this._updatingPromise = undefined;
 		}
 	}
 
